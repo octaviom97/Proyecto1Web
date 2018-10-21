738b7880430802209c0fa11475551ffd386eff60
tavo <tavo@localhost.localdomain>
2018-10-21 11:40:16 -0500

    Curriculum 1.0

---
+++ mensaje.php
@@ -0,0 +1,11 @@
+<?php
+
+$destino = "octaviiom97@gmail.com";
+$nombre=$_POST["nombre"];
+$correo=$_POST["correo"];
+$mensaje=$_POST["mensaje"];
+$contenido="Nombre: ".$nombre."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
+
+mail($destino,"Correo desde php",$contenido);
+
+?>
